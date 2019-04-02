55ecdb2998aec6fee67ee7fbdb2ed983bf1003d7
Jan von der Assen <jan.vda@gmx.ch>
2019-04-02 12:28:36 +0200

    Add unit tests for all stopword providers

---
+++ src/test/java/ch/uzh/rackrec/nlp/ModifiedStopWordProviderTest.java
@@ -0,0 +1,48 @@
+package ch.uzh.rackrec.nlp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class ModifiedStopWordProviderTest {
+    IStopWordProvider sut;
+    String validStopWord;
+    String invalidStopWord;
+    String emptyString;
+
+    @Before
+    public void initialize(){
+        validStopWord = "the";
+        invalidStopWord = "from";
+        emptyString = "";
+
+        sut = new ModifiedStopwordProvider();
+    }
+
+    @Test
+    public void getAllStopWords(){
+        List<String> allStopWords = sut.getStopWords();
+        assertTrue(allStopWords.size() > 0);
+    }
+
+    @Test
+    public void isAStopWord(){
+        boolean isStopWord = sut.isStopWord(validStopWord);
+        assertTrue(isStopWord);
+    }
+
+    @Test
+    public void isNotAStopWord(){
+        boolean isNotStopWord = !sut.isStopWord(invalidStopWord);
+        assertTrue(isNotStopWord);
+    }
+
+    @Test
+    public void testEmptyString(){
+        boolean isNotStopWord = !sut.isStopWord(emptyString);
+        assertTrue(isNotStopWord);
+    }
+}
