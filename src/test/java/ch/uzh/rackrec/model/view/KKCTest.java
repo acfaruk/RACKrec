02bd0d7f3d28e82aba2b84d9d26123242ab1e841
SpielmannCode <23359516+SpielmannCode@users.noreply.github.com>
2019-04-04 15:40:33 +0200

    Added Test for KKC

--- src/test/java/ch/uzh/rackrec/model/view/KKCTest.java
+++ src/test/java/ch/uzh/rackrec/model/view/KKCTest.java
@@ -1,4 +1,42 @@
 package ch.uzh.rackrec.model.view;
 
+import cc.kave.commons.model.naming.IName;
+import cc.kave.commons.model.naming.impl.v0.types.organization.AssemblyName;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertTrue;
+
 public class KKCTest {
+    KKC sut;
+    String keyword1;
+    String keyword2;
+    Map.Entry<String, String> keywordpair;
+    List<IName> apis;
+    Double score;
+
+
+    @Before
+    public void initialize(){
+        keyword1 = "Test";
+        keyword2 = "Passed";
+        keywordpair = new AbstractMap.SimpleEntry<>(keyword1,keyword2);
+        apis.add(new AssemblyName("mscorlib"));
+        score = 0.86547;
+    }
+    @Test
+    public void test(){
+        sut = new KKC(keywordpair,apis,score);
+        boolean keywordsAreTrue = sut.getKeywordPair() == keywordpair;
+        boolean apiListIsTrue = sut.getApis() == apis;
+        boolean scoreIsTrue = sut.getScore() == score;
+        assertTrue(keywordsAreTrue);
+        assertTrue(apiListIsTrue);
+        assertTrue(scoreIsTrue);
+    }
 }
