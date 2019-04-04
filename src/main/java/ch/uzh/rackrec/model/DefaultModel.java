bd2b5b1d29a49ccd5f1e80cad186227788f2778d
lucBoillat <luc.boillat@outlook.com>
2019-04-04 12:29:57 +0200

    Implement logger and update lemmatizer test

--- src/main/java/ch/uzh/rackrec/model/DefaultModel.java
+++ src/main/java/ch/uzh/rackrec/model/DefaultModel.java
@@ -4,11 +4,12 @@ import ch.uzh.rackrec.model.gen.ModelGenerator;
 import com.google.inject.Inject;
 
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class DefaultModel extends Model {
 
     @Inject
-    public DefaultModel(Properties properties, ModelGenerator modelGenerator) {
-        super(properties, modelGenerator);
+    public DefaultModel(Properties properties, ModelGenerator modelGenerator, Logger logger) {
+        super(properties, modelGenerator, logger);
     }
 }
