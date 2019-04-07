7d825c5f43252da623f4613ed37c610be21285bd
Faruk Acibal <acfaruk@gmail.com>
2019-04-07 18:32:07 +0200

    make example work again

--- example/src/main/java/ch/uzh/rackrec/example/Example.java
+++ example/src/main/java/ch/uzh/rackrec/example/Example.java
@@ -1,15 +1,22 @@
 package ch.uzh.rackrec.example;
 
 import ch.uzh.rackrec.rec.DefaultRecommender;
-import ch.uzh.rackrec.rec.config.DefaultModule;
+import ch.uzh.rackrec.rec.config.KaveContextModule;
+
+import java.util.Properties;
 
 public class Example {
 
     public static void main(String[] args) {
-        System.out.println("Test");
 
-        DefaultRecommender rec = new DefaultRecommender(DefaultModule.create());
+        //set up properties object, see the readme for available properties depending on the module type
+        Properties props = new Properties();
+        props.setProperty("test", "test");
 
-        //rec.query()
+        //choose your module, see the readme for available modules
+        KaveContextModule module = new KaveContextModule(props);
+
+        //create your Recommender, see the readme for available recommenders
+        DefaultRecommender rec = new DefaultRecommender(module);
     }
 }
