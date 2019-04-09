f286789f3893cdcfbfaf96653b96308f73779724
Faruk Acibal <acfaruk@gmail.com>
2019-04-09 12:23:57 +0200

    add failing zip reading test

--- src/test/java/ch/uzh/rackrec/data/IoHelperTest.java
+++ src/test/java/ch/uzh/rackrec/data/IoHelperTest.java
@@ -3,6 +3,7 @@ package ch.uzh.rackrec.data;
 import cc.kave.commons.model.events.completionevents.Context;
 import org.junit.Test;
 
+import java.io.StringReader;
 import java.net.URL;
 import java.util.List;
 import java.util.Set;
@@ -12,7 +13,7 @@ import static org.junit.Assert.*;
 public class IoHelperTest {
 
     @Test
-    public void readZipFileTest(){
+    public void readZipFile(){
         URL resource = IoHelperTest.class.getResource("/context/test.zip");
         List<Context> contexts = IoHelper.read(resource.getPath());
 
@@ -20,11 +21,18 @@ public class IoHelperTest {
     }
 
     @Test
-    public void findAllZipsTest(){
+    public void findAllZips(){
         URL resource = IoHelperTest.class.getResource("/context");
         Set<String> zipDirs = IoHelper.findAllZips(resource.getPath());
 
         assertEquals(1, zipDirs.size());
         assertEquals(zipDirs.iterator().next(), "test.zip");
+    }
+
+    @Test
+    public void failReadingZipFile(){
+        List<Context> zipDirs = IoHelper.read("wrong");
+
+        assertEquals(0, zipDirs.size());
     }
 }
