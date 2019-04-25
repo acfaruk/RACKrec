8b27f6337dd3fd1c70e276a71f0e86b41a80ec21
Jan von der Assen <jan.vda@gmx.ch>
2019-04-25 18:55:57 +0200

    Pull SQLProvider test into four smaller test classes

--- src/test/java/ch/uzh/rackrec/model/provider/SQLiteProviderAccessorsTest.java
+++ src/test/java/ch/uzh/rackrec/model/provider/SQLiteProviderAccessorsTest.java
@@ -2,15 +2,13 @@ package ch.uzh.rackrec.model.provider;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.sql.SQLException;
 import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import org.junit.After;
 import org.junit.Before;
@@ -20,15 +18,23 @@ import static org.mockito.Mockito.when;
 
 import cc.kave.commons.model.naming.impl.v0.types.TypeName;
 
-public class SQLiteProviderTest {
+public class SQLiteProviderAccessorsTest {
     String testingDB = "./testdb.db";
     SQLiteProvider provider;
+    String[] tokens = {"compu", "md5", "hash"};
+    String[] apis = { "otherclass.othermethod","someclass.somemethod", "otherclass.othermethod"};
+    TypeName typeName = mock(TypeName.class);
+    Logger logger = mock(Logger.class);
+    ModelEntry entry;
 
     @Before
-    public void initialize() {
+    public void initialize() throws Exception {
         Properties settings = new Properties();
         settings.put("database-file", testingDB);
-        provider = new SQLiteProvider(settings);
+        provider = new SQLiteProvider(settings, logger);
+        provider.prepareSchemas();
+        entry = new ModelEntry(Arrays.asList(tokens), Arrays.asList(apis), typeName);
+        provider.saveMinedContext(entry);
     }
     
     @After
@@ -39,232 +45,21 @@ public class SQLiteProviderTest {
     }
 
     @Test
-    public void testPrepareStoreTokens() {
-        String[] tokens = {"comput", "MD5", "Hash"};
-        String sqlStatement = provider.prepareStoreTokens(Arrays.asList(tokens));
-        String expected = "INSERT OR IGNORE INTO tokens (Token) VALUES (\"comput\"),\n(\"MD5\"),\n(\"Hash\");";
-    
-        assertEquals(sqlStatement, expected);
-    }
-
-    @Test
-    public void testPrepareStoreAPI() {
-        String[] apis = {"someclass.somemethod", "otherclass.othermethod"};
-        List<String> testApis = new ArrayList<String>(Arrays.asList(apis));
-        String sqlStatement = provider.prepareStoreAPI(testApis);
-        String expected = "INSERT OR IGNORE INTO apis (API) VALUES (\"someclass.somemethod\"),\n(\"otherclass.othermethod\");";
-        System.out.println(sqlStatement);
-    
-        assertEquals(sqlStatement, expected);
-    }
-    @Test
-    public void testCheckTableExists() throws SQLException {
-        boolean tableDoesNotExist = !provider.tableExists("testTable123");
-        
-        assertTrue(tableDoesNotExist);
-    }
-    
-    @Test
-    public void testCreateSchemas() {
-        boolean tableDoesExist = false;
-        boolean apiTableDoesExist = false;
-        try {
-            provider.prepareSchemas();
-            tableDoesExist = provider.tableExists("contexts");
-            apiTableDoesExist = provider.tableExists("apis");
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        assertTrue(apiTableDoesExist);
-        assertTrue(tableDoesExist);
-    }
-    
-    @Test
-    public void testMultipleCreation() throws SQLException {
-        provider.prepareSchemas();
-        boolean tableDoesExist = provider.tableExists("contexts");
-        assertTrue(tableDoesExist);
-        try { 
-            provider.prepareSchemas();
-            assertTrue(tableDoesExist);
-        } catch(Exception e) {
-            fail("Creation methods should check if the table to be created exists");
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void testStoreApis() throws SQLException {
-        provider.prepareSchemas();
-        assertTrue(provider.tableExists("apis"));
-        String[] apis = {"someclass.somemethod", "otherclass.othermethod", "otherclass.othermethod"};
-        provider.storeAPIS(Arrays.asList(apis));
-        provider.storeAPIS(Arrays.asList(apis));
-
-        List<String> apiRows = provider.getAPIs();
-        int actualLength = apiRows.size();
-        int expectedLength = 2;
-        assertEquals(expectedLength, actualLength);
-    }
-
-    @Test
-    public void testStoreTokens() throws SQLException {
-        provider.prepareSchemas();
-        assertTrue(provider.tableExists("tokens"));
-        String[] tokens = {"compu", "md5", "hash"};
-        String[] duplicateTokens = {"hash"};
-        provider.storeTokens(Arrays.asList(tokens));
-        provider.storeTokens(Arrays.asList(duplicateTokens));
-        
-        List<String> tokenRows = provider.getTokens();
-        int actualLength = tokenRows.size();
-        int expectedLength = 3;
-        assertEquals(expectedLength, actualLength);
-    }
-    
-    @Test
-    public void testStoreNewApiReference() throws SQLException {
-        provider.prepareSchemas();
-        
-        TypeName testContext = mock(TypeName.class);
-        String[] apis = {"someLib.someMethod", "otherLib.otherMethod"};
-        when(testContext.toString()).thenReturn("someClientClass.someMethod");
-        provider.storeAPIS(Arrays.asList(apis));
-        provider.storeContext(testContext);
-        provider.saveApiContextReference("someLib.someMethod", testContext);
-        List<String> storedReferences = provider.getApisForContext(testContext.toString());
-        int actualNumOfReferences = storedReferences.size();
-        int expectedNumOfReferences = 1;
-        assertEquals(expectedNumOfReferences, actualNumOfReferences);
-
-        assertTrue(provider.apiContextReferenceExists("someLib.someMethod", testContext));
-    }
-
-    @Test
-    public void testStoreNewTokenReference() throws SQLException {
-        provider.prepareSchemas();
-        
-        TypeName testContext = mock(TypeName.class);
-        String[] tokens = {"compu", "MD5", "compu"};
-        when(testContext.toString()).thenReturn("someClientClass.someMethod");
-        provider.storeTokens(Arrays.asList(tokens));
-        provider.storeContext(testContext);
-        provider.saveTokenContextReference(Arrays.asList(tokens), testContext);
-
-        List<String> storedReferences = provider.getTokensForContext(testContext.toString());
-        int actualNumOfReferences = storedReferences.size();
-        int expectedNumOfReferences = 2;
-        assertEquals(expectedNumOfReferences, actualNumOfReferences);
-    }
-
-    @Test
-    public void testStoreExistingApiReference() throws SQLException {
-        provider.prepareSchemas();
-
-        TypeName testContext = mock(TypeName.class);
-        String[] apis = {"someLib.someMethod", "otherLib.otherMethod"};
-        when(testContext.toString()).thenReturn("someClientClass.someMethod");
-        provider.storeAPIS(Arrays.asList(apis));
-        provider.storeContext(testContext);
-        provider.saveApiContextReference("someLib.someMethod", testContext);
-        List<String> storedReferences = provider.getApisForContext(testContext.toString());
-
-        int actualNumOfReferences = storedReferences.size();
-        int expectedNumOfReferences = 1;
-
-        assertEquals(expectedNumOfReferences, actualNumOfReferences);
-        assertTrue(provider.apiContextReferenceExists("someLib.someMethod", testContext));
-        assertEquals(1, provider.getApiCountForContext(testContext.toString(),"someLib.someMethod" ));
-
-        provider.saveApiContextReference("someLib.someMethod", testContext);
-        storedReferences = provider.getApisForContext(testContext.toString());
-        actualNumOfReferences = storedReferences.size();
-        assertEquals(expectedNumOfReferences, actualNumOfReferences);
-        assertEquals(2, provider.getApiCountForContext(testContext.toString(),"someLib.someMethod" ));
-        
-        String[] multipleApis = {"someLib.someMethod", "someLib.someMethod"};
-        provider.saveApiContextReference(Arrays.asList(multipleApis), testContext);
-        assertEquals(4, provider.getApiCountForContext(testContext.toString(),"someLib.someMethod" ));
-    }
-
-    @Test
-    public void testStoreMultipleExistingApiReferences() throws SQLException {
-        provider.prepareSchemas();
-
-        TypeName testContext = mock(TypeName.class);
-        String[] apis = {"someLib.someMethod", "otherLib.otherMethod"};
-        when(testContext.toString()).thenReturn("someClientClass.someMethod");
-        provider.storeAPIS(Arrays.asList(apis));
-        provider.storeContext(testContext);
-        provider.saveApiContextReference("someLib.someMethod", testContext);
-
-        String[] multipleApis = {"someLib.someMethod", "someLib.someMethod"};
-        provider.saveApiContextReference(Arrays.asList(multipleApis), testContext);
-        assertEquals(3, provider.getApiCountForContext(testContext.toString(),"someLib.someMethod" ));
-    }
-
-    @Test
-    public void testStoreModelEntry() throws Exception {
-        provider.prepareSchemas();
-
-        String[] tokens = {"compu", "md5", "hash"};
-        String[] apis = {"someclass.somemethod", "otherclass.othermethod", "otherclass.othermethod"};
-        TypeName typeName = mock(TypeName.class);
-        
-        ModelEntry entry = new ModelEntry(Arrays.asList(tokens), Arrays.asList(apis), typeName);
-
-        provider.saveMinedContext(entry);
-    
-        List<String> tokenRows = provider.getTokens();
-        int nrOfTokens = tokenRows.size();
-        int expectedNrOfTokens = tokens.length;
-        assertEquals(expectedNrOfTokens, nrOfTokens);
-
-        List<String> apiRows = provider.getAPIs();
-        int nrOfAPIs = apiRows.size();
-        int expectedNrOfApis = 2;
-        assertEquals(expectedNrOfApis, nrOfAPIs);
-
-        List<String> contextRows = provider.getContexts();
-        int nrOfContexts = contextRows.size();
-        int expectedNrOfContexts = 1;
-        assertEquals(expectedNrOfContexts, nrOfContexts);
-
-        List<String> contextForApiRows = provider.getApisForContext(typeName.toString());
-        int nrOfApisForCtx = contextForApiRows.size();
-        int expectedNrOfApisForCtx = 2;
-        assertEquals(expectedNrOfApisForCtx, nrOfApisForCtx);
-    }
-
-    @Test
     public void testgetTopKEntries() throws Exception {
-        provider.prepareSchemas();
-
-        String[] tokens = {"compu", "md5", "hash"};
-        String[] apis = { "otherclass.othermethod","someclass.somemethod", "otherclass.othermethod"};
-        TypeName typeName = mock(TypeName.class);
-        
-        ModelEntry entry = new ModelEntry(Arrays.asList(tokens), Arrays.asList(apis), typeName);
-
-        provider.saveMinedContext(entry);
-
-        String[] tokensCTX2 = {"compu", "md5", "hash"};
+        String[] tokensCTX2 = tokens;
         String[] apisCTX2 = { "otherclass.othermethod", "someclass.somemethod"};
         TypeName typeNameCTX2 = mock(TypeName.class);
         when(typeNameCTX2.toString()).thenReturn("client2");
         
         ModelEntry entryCTX2 = new ModelEntry(Arrays.asList(tokensCTX2), Arrays.asList(apisCTX2), typeNameCTX2);
+		provider.saveMinedContext(entryCTX2);
 
-        provider.saveMinedContext(entryCTX2);
-
-        String[] tokensCTX3 = {"compu", "md5", "hash"};
+        String[] tokensCTX3 = tokens;
         String[] apisCTX3 = {"someclass.somemethod", "someclass.somemethod", "someclass.somemethod", "someclass.somemethod", "thirdAPI.thridmeth", "fourthAPI.fourthmeth", "fifthAPI.fifthMeth" };
         TypeName typeNameCTX3 = mock(TypeName.class);
         when(typeNameCTX3.toString()).thenReturn("client3");
         
         ModelEntry entryCTX3 = new ModelEntry(Arrays.asList(tokensCTX3), Arrays.asList(apisCTX3), typeNameCTX3);
-
         provider.saveMinedContext(entryCTX3);
 
         String[] tokensCTX4 = {"bla"};
@@ -273,7 +68,6 @@ public class SQLiteProviderTest {
         when(typeNameCTX4.toString()).thenReturn("client4");
         
         ModelEntry entryCTX4 = new ModelEntry(Arrays.asList(tokensCTX4), Arrays.asList(apisCTX4), typeNameCTX4);
-
         provider.saveMinedContext(entryCTX4);
     
         List<String> tokenRows = provider.getTokens();
@@ -306,16 +100,6 @@ public class SQLiteProviderTest {
     }
     @Test
     public void testGetTokensForAPI() throws Exception {
-        provider.prepareSchemas();
-
-        String[] tokens = {"compu", "md5", "hash"};
-        String[] apis = {"someclass.somemethod", "otherclass.othermethod", "otherclass.othermethod"};
-        TypeName typeName = mock(TypeName.class);
-        
-        ModelEntry entry = new ModelEntry(Arrays.asList(tokens), Arrays.asList(apis), typeName);
-
-        provider.saveMinedContext(entry);
-    
         List<String> tokenRows = provider.getTokens();
         int nrOfTokens = tokenRows.size();
         int expectedNrOfTokens = tokens.length;
---
+++ src/test/java/ch/uzh/rackrec/model/provider/SQLiteProviderInitializationTest.java
@@ -0,0 +1,102 @@
+package ch.uzh.rackrec.model.provider;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class SQLiteProviderInitializationTest {
+    String testingDB = "./testdb.db";
+    SQLiteProvider provider;
+    String inexistentDatabase = "thisDBDoesNotExist";
+    String contextDBName = "contexts";
+    String apiDBName = "apis";
+    Logger logger = mock(Logger.class);
+
+    @Before
+    public void initialize() {
+        Properties settings = new Properties();
+        settings.put("database-file", testingDB);
+        provider = new SQLiteProvider(settings, logger);
+    }
+    
+    @After
+    public void deleteDB() {
+        provider.closeConnection();
+        File file = new File(testingDB); 
+        file.delete();
+    }
+
+    @Test
+    public void testPrepareStoreTokens() {
+        String[] tokens = {"comput", "MD5", "Hash"};
+
+        String sqlStatement = provider.prepareStoreTokens(Arrays.asList(tokens));
+        String expected = "INSERT OR IGNORE INTO tokens (Token) VALUES (\"comput\"),\n(\"MD5\"),\n(\"Hash\");";
+    
+        assertEquals(sqlStatement, expected);
+    }
+
+    @Test
+    public void testPrepareStoreAPI() {
+        String[] apis = {"someclass.somemethod", "otherclass.othermethod"};
+
+        List<String> testApis = new ArrayList<String>(Arrays.asList(apis));
+        String sqlStatement = provider.prepareStoreAPI(testApis);
+        String expected = "INSERT OR IGNORE INTO apis (API) VALUES (\"someclass.somemethod\"),\n(\"otherclass.othermethod\");";
+        System.out.println(sqlStatement);
+    
+        assertEquals(sqlStatement, expected);
+    }
+    @Test
+    public void testCheckTableExists() throws SQLException {
+        boolean tableDoesNotExist = !provider.tableExists(inexistentDatabase);
+        
+        assertTrue(tableDoesNotExist);
+    }
+    
+    @Test
+    public void testCreateSchemas() {
+        boolean tableDoesExist = false;
+        boolean apiTableDoesExist = false;
+
+        try {
+            provider.prepareSchemas();
+            tableDoesExist = provider.tableExists(contextDBName);
+            apiTableDoesExist = provider.tableExists(apiDBName);
+        } catch (SQLException e) {
+        	fail(e.getMessage());
+            e.printStackTrace();
+        }
+
+        assertTrue(apiTableDoesExist);
+        assertTrue(tableDoesExist);
+    }
+    
+    @Test
+    public void testMultipleCreation() throws SQLException {
+        provider.prepareSchemas();
+        boolean tableDoesExist = provider.tableExists(contextDBName);
+        assertTrue(tableDoesExist);
+        try { 
+            provider.prepareSchemas();
+            assertTrue(tableDoesExist);
+        } catch(Exception e) {
+            fail("Creation methods should check if the table to be created exists");
+            e.printStackTrace();
+        }
+    }
+}
