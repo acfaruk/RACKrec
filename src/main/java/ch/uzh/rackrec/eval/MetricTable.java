ba736c831c0b2448462b0e4a1fae204d3a7009c1
lucBoillat <luc.boillat@outlook.com>
2019-04-27 17:38:03 +0200

    add metricTable data structure

---
+++ src/main/java/ch/uzh/rackrec/eval/MetricTable.java
@@ -0,0 +1,12 @@
+package ch.uzh.rackrec.eval;
+
+public class MetricTable {
+		
+	public MetricTable(Integer k) {
+		
+	}
+	
+	public void printTable() {
+		System.out.println("this is a test print statement");
+	}
+}
--- src/main/java/ch/uzh/rackrec/eval/Metrics.java
+++ src/main/java/ch/uzh/rackrec/eval/Metrics.java
@@ -1,5 +1,44 @@
 package ch.uzh.rackrec.eval;
 
-public class Metrics {
+import java.util.Set;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import cc.kave.commons.model.naming.codeelements.IMemberName;
+
+public class Metrics {
+	
+	private Set<Pair<IMemberName, Double>> resultRACK;
+	private Set<Pair<IMemberName, Double>> resultGold;
+	private Integer maxK;
+	
+	public MetricTable metricTable = new MetricTable(maxK);
+	
+	public Metrics(Set<Pair<IMemberName, Double>> resultRACK, Set<Pair<IMemberName, Double>> resultGold, Integer maxK) {
+		this.resultRACK = resultRACK;
+		this.resultGold = resultGold;
+		this.maxK = maxK;
+	}
+	
+	public MetricTable calculateMetricTable() {
+		
+		return metricTable;
+	}
+	
+	
+	private double calculateTopKAccuracy() {
+		return 0.0;
+	}
+	
+	private double calculateMeanReciprocalRank() {
+		return 0.0;
+	}
+
+	private double calculateMeanAveragePrecision() {
+		return 0.0;
+	}
+	
+	private double calculateMeanRecall() {
+		return 0.0;
+	}
 }
